29d84b259d59b9c6252671f68e0fcb47448ddda5
Matthew Grayum <mattgrayum@gmail.com>
2018-02-19 09:09:41 -0800

    Put the block of code in Default.aspx Page Load method that populates the fields with session variables inside an If Not IsPostBack statement so that we can go back to the form, change values, and have those values update their session variable and be reflected on the Results page.

--- Assignment_1/Assignment_1/Default.aspx.vb
+++ Assignment_1/Assignment_1/Default.aspx.vb
@@ -59,32 +59,34 @@ Partial Class _Default
     Sub Page_Load(ByVal Sender As Object, ByVal E As EventArgs) Handles MyBase.Load
 
         ' Set each input field to the value of the associated Session variable
-        If Session("numberOfTaxpayers") IsNot Nothing Then
-            lstIndividualOrJoint.SelectedIndex = Session("numberOfTaxpayers") - 1
-        End If
-        If Session("wages") IsNot Nothing Then
-            txtWages.Text = Session("wages")
-        End If
-        If Session("interest") IsNot Nothing Then
-            txtInterest.Text = Session("interest")
-        End If
-        If Session("unemployment") IsNot Nothing Then
-            txtUnemployment.Text = Session("unemployment")
-        End If
-        If Session("you") IsNot Nothing Then
-            chkYou.Checked = Session("you")
-        End If
-        If Session("spouse") IsNot Nothing Then
-            chkSpouse.Checked = Session("spouse")
-        End If
-        If Session("withholding") IsNot Nothing Then
-            txtWithholding.Text = Session("withholding")
-        End If
-        If Session("eic") IsNot Nothing Then
-            txtEarnedIncome.Text = Session("eic")
-        End If
-        If Session("nontaxable") IsNot Nothing Then
-            txtNontaxable.Text = Session("nontaxable")
+        If Not IsPostBack Then
+            If Session("numberOfTaxpayers") IsNot Nothing Then
+                lstIndividualOrJoint.SelectedIndex = Session("numberOfTaxpayers") - 1
+            End If
+            If Session("wages") IsNot Nothing Then
+                txtWages.Text = Session("wages")
+            End If
+            If Session("interest") IsNot Nothing Then
+                txtInterest.Text = Session("interest")
+            End If
+            If Session("unemployment") IsNot Nothing Then
+                txtUnemployment.Text = Session("unemployment")
+            End If
+            If Session("you") IsNot Nothing Then
+                chkYou.Checked = Session("you")
+            End If
+            If Session("spouse") IsNot Nothing Then
+                chkSpouse.Checked = Session("spouse")
+            End If
+            If Session("withholding") IsNot Nothing Then
+                txtWithholding.Text = Session("withholding")
+            End If
+            If Session("eic") IsNot Nothing Then
+                txtEarnedIncome.Text = Session("eic")
+            End If
+            If Session("nontaxable") IsNot Nothing Then
+                txtNontaxable.Text = Session("nontaxable")
+            End If
         End If
 
     End Sub
