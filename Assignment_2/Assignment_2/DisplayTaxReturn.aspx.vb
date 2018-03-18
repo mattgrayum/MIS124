22d22a90788b1f934d3383858aa534be16415a6b
Matthew Grayum <mattgrayum@gmail.com>
2018-03-17 17:57:28 -0700

    A2 added Result.aspx page and fleshed it out with all of its controls (just copied and pasted from Assignment 1) and hooked up all navigation buttons

--- Assignment_2/Assignment_2/DisplayTaxReturn.aspx.vb
+++ Assignment_2/Assignment_2/DisplayTaxReturn.aspx.vb
@@ -2,4 +2,8 @@
 Partial Class DisplayTaxReturn
     Inherits System.Web.UI.Page
 
+    Protected Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        ' Redirect to the Result Page
+        Response.Redirect(url:="~/Result.aspx", endResponse:=False)
+    End Sub
 End Class
---
+++ Assignment_2/Assignment_2/Result.aspx.vb
@@ -0,0 +1,9 @@
+﻿
+Partial Class Result
+    Inherits System.Web.UI.Page
+
+    Protected Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnBack.Click
+        ' Redirect back to the DisplayTaxReturn Page
+        Response.Redirect(url:="~/DisplayTaxReturn.aspx", endResponse:=False)
+    End Sub
+End Class
