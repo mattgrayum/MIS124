b6e948364460b5ac98a0a3ab76c20fed9e40de76
Matthew Grayum <mattgrayum@gmail.com>
2018-03-17 18:13:35 -0700

    A2 hooked up the 'go back' button on the middle page

--- Assignment_2/Assignment_2/DisplayTaxReturn.aspx.vb
+++ Assignment_2/Assignment_2/DisplayTaxReturn.aspx.vb
@@ -6,4 +6,8 @@ Partial Class DisplayTaxReturn
         ' Redirect to the Result Page
         Response.Redirect(url:="~/Result.aspx", endResponse:=False)
     End Sub
+    Protected Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnBack.Click
+        ' Redirect back to the Default Page
+        Response.Redirect(url:="~/Default.aspx", endResponse:=False)
+    End Sub
 End Class
