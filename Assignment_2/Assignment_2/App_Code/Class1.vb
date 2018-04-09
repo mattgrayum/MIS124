e6aa58ebc28656b0e7799861cc7f18c7cf2c9026
Matthew Grayum <mattgrayum@gmail.com>
2018-04-09 06:33:22 -0700

    A2 created a database connection class and started creating a tax payer class

---
+++ Assignment_2/Assignment_2/App_Code/Class1.vb
@@ -0,0 +1,13 @@
+﻿Imports Microsoft.VisualBasic
+
+Public Class clsTaxPayer
+    Private ReadOnly mstrTaxPayerID As Integer
+    Private ReadOnly mstrLastName As String
+    Private ReadOnly mstrFirstName As String
+    Private ReadOnly mstrMidInitial As String
+    Private ReadOnly mstrAddress As String
+    Private ReadOnly mstrCity As String
+    Private ReadOnly mstrState As String
+    Private ReadOnly mstrZip As String
+    Private ReadOnly mstrTimeCreated As DateAndTime
+End Class
---
+++ Assignment_2/Assignment_2/App_Code/clsDBConnection.vb
@@ -0,0 +1,7 @@
+﻿Imports Microsoft.VisualBasic
+
+Public Class clsDBConnection
+    Public Shared Function getConnection() As Data.SqlClient.SqlConnection
+        Return New Data.SqlClient.SqlConnection(ConfigurationManager.ConnectionStrings("TaxReturn2014_Rodger").ConnectionString)
+    End Function
+End Class
