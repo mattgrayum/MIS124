c854e4b8fd522c46408c728e780d1fa4f648ed97
Matthew Grayum <mattgrayum@gmail.com>
2018-04-09 06:43:52 -0700

    A2 added a tax payer class

--- Assignment_2/Assignment_2/App_Code/Class1.vb
+++ Assignment_2/Assignment_2/App_Code/Class1.vb
@@ -9,5 +9,84 @@ Public Class clsTaxPayer
     Private ReadOnly mstrCity As String
     Private ReadOnly mstrState As String
     Private ReadOnly mstrZip As String
-    Private ReadOnly mstrTimeCreated As DateAndTime
+    Private ReadOnly mstrTimeCreated As Date
+
+    Public ReadOnly Property TaxPayerID As Integer
+        Get
+            Return mstrTaxPayerID
+        End Get
+    End Property
+
+    Public ReadOnly Property LastName As String
+        Get
+            Return mstrLastName
+        End Get
+    End Property
+
+    Public ReadOnly Property FirstName As String
+        Get
+            Return mstrFirstName
+        End Get
+    End Property
+
+    Public ReadOnly Property MidInitial As String
+        Get
+            Return mstrMidInitial
+        End Get
+    End Property
+
+    Public ReadOnly Property Address As String
+        Get
+            Return mstrAddress
+        End Get
+    End Property
+
+    Public ReadOnly Property City As String
+        Get
+            Return mstrCity
+        End Get
+    End Property
+
+    Public ReadOnly Property State As String
+        Get
+            Return mstrState
+        End Get
+    End Property
+
+    Public ReadOnly Property Zip As String
+        Get
+            Return mstrZip
+        End Get
+    End Property
+
+    Public ReadOnly Property TimeCreated As Date
+        Get
+            Return mstrTimeCreated
+        End Get
+    End Property
+
+    Public Sub New()
+
+    End Sub
+
+    Public Sub New(ByVal intID As Integer,
+                   ByVal strLName As String,
+                   ByVal strFName As String,
+                   ByVal strMInit As String,
+                   ByVal strAddr As String,
+                   ByVal strCity As String,
+                   ByVal strState As String,
+                   ByVal strZip As String,
+                   ByVal dtTimeCreated As Date)
+
+        mstrTaxPayerID = intID
+        mstrLastName = strLName
+        mstrFirstName = strFName
+        mstrMidInitial = strMInit
+        mstrAddress = strAddr
+        mstrCity = strCity
+        mstrState = strState
+        mstrZip = strZip
+        mstrTimeCreated = dtTimeCreated
+    End Sub
 End Class
